a788909900b2140c00bab4ad103b5558ac13c694
Puja Singh <pujasingh8942@gmail.com>
2025-04-21 14:51:35 +0530

    Vite.Config Modification @nd time

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SKYCAST---Weather-Application /', // 👈 Add this line
+  base: '/',
   plugins: [react()],
 })
